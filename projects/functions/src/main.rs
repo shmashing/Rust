420d302a8ce462bd89f64d620eab48978af42f46
Kyle <kyle.mcfar1291@gmail.com>
2017-11-08 15:33:48 -0700

    playing around with functions

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let x = five();
+    println!("the value of x: {}",x);
+
+    let x = add_one(five());
+    println!("the value of x: {}",x);
+
+    let x = hello_world();
+    println!("the value of x: {}",x);
+}
+fn five() -> i32 {
+    5
+}
+fn add_one(x: i32) -> i32 {
+    x+1
+}
+fn hello_world() -> &'static str {
+    println!("in hello world funct");
+    "Hello World"
 }
---
+++ projects/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/Kyle/Desktop/School/Programming/Rust/projects/functions/target/debug/functions: /Users/Kyle/Desktop/School/Programming/Rust/projects/functions/src/main.rs
