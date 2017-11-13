4adb5e8943f02c95863490166dcbc41819fd45d7
Kyle <kyle.mcfar1291@gmail.com>
2017-11-13 14:13:58 -0700

    more play with random numbers and rust

--- projects/random/src/main.rs
+++ projects/random/src/main.rs
@@ -1,13 +1,17 @@
 extern crate rand;
 
-use rand::Rng;
+use rand::distributions::{IndependentSample, Range};
 
 fn main() {
+    let between = Range::new(5,11);
     let mut rng = rand::thread_rng();
 
     for _ in 1..100 {
-        let rand_num = rand::random::<u8>;
-        println!("random number: {}", rand_num as u16);
+
+        let x = between.ind_sample(&mut rng);
+        let y = between.ind_sample(&mut rng);
+        println!("random number1: {}", x as u16);
+        println!("random number2: {}", y as u16)
     
     }
 }
